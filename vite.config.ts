0615138414975cf2656c6c36355d6b9ccf05b7c4
Shuashuaa <Shuashuaa@users.noreply.github.com>
2026-02-26 13:58:28 +0800

    feat(vite): Configure Tailwind CSS PostCSS integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/postcss'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss]
+    }
+  }
 })
